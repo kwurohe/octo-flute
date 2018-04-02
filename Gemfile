51975b296eccd31530a3aedaa8c24a3a2eb23610
Keren WuRohe <kerenrohe@gmail.com>
2018-04-02 23:37:15 +0000

    Nomster Lesson 16

--- Gemfile
+++ Gemfile
@@ -63,3 +63,4 @@ end
 gem 'kaminari'
 gem 'bootstrap-kaminari-views'
 gem "font-awesome-rails"
+gem 'devise'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.action_mailer.default_url_options = { host: 'limitless-beach-52895.herokuapp.com/' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
