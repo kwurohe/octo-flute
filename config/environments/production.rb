e74ffa2d008e116bbdc9bd43d769eefbc79f5e86
Keren WuRohe <kerenrohe@gmail.com>
2018-05-07 18:55:49 -0400

    Nomster Lesson 44 Take 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,13 +1,13 @@
 Rails.application.configure do
   
   ActionMailer::Base.smtp_settings = {
-  :user_name => ENV['SENDGRID_USERNAME'],
-  :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'yourdomain.com',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
+    address: 'smtp.sendgrid.net',
+    port: '587',
+    authentication: :plain,
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'heroku.com',
+    enable_starttls_auto: true
   }
   
   config.action_mailer.default_url_options = { host: 'limitless-beach-52895.herokuapp.com/' }
